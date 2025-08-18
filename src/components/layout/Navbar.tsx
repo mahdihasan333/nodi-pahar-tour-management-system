bb43812ba0071cca4b4bce88480eb96caf0db369
Mahdi Hassan <mahdioffi333@gmail.com>
2025-08-18 20:35:00 +0600

    Recap and Discussion on Completed Work

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -22,8 +22,10 @@ import { useAppDispatch } from "@/redux/hook";
 
 // Navigation links array to be used in both desktop and mobile menus
 const navigationLinks = [
-  { href: "/", label: "Home" },
-  { href: "/about", label: "About" },
+  { href: "/", label: "Home", role: "PUBLIC" },
+  { href: "/about", label: "About", role: "PUBLIC" },
+  { href: "/admin", label: "Dashboard", role: role.admin },
+  { href: "/user", label: "Dashboard", role: role.user },
 ];
 
 export default function Navbar() {
