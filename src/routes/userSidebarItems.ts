5178cdf477deb41168c4caaa2cc7e5569a53cbcb
Mahdi Hassan <mahdioffi333@gmail.com>
2025-08-18 10:08:09 +0600

    Rendering Sidebar Items Based on User Role

---
+++ src/routes/userSidebarItems.ts
@@ -0,0 +1,16 @@
+
+import Bookings from "@/pages/user/Bookings";
+import { ISidebarItem } from "@/types";
+
+export const userSidebarItems: ISidebarItem[] = [
+  {
+    title: "History",
+    items: [
+      {
+        title: "Bookings",
+        url: "/user/bookings",
+        component: Bookings,
+      },
+    ],
+  },
+];
