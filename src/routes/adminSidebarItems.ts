c4818cdb6b2ff0d0c42e7fa4a02b0866b66c2877
Mahdi Hassan <mahdioffi333@gmail.com>
2025-08-19 18:32:52 +0600

    Adding “Tour Type” Feature and Updating the Corresponding Backend Endpoint

--- src/routes/adminSidebarItems.ts
+++ src/routes/adminSidebarItems.ts
@@ -1,10 +1,9 @@
 import AddTour from "@/pages/Admin/AddTour";
 import AddTourType from "@/pages/Admin/AddTourType";
-// import Analytics from "@/pages/Admin/Analytics";
 import { ISidebarItem } from "@/types";
 import { lazy } from "react";
 
-const Analytics = lazy(() => import("@/pages/Admin/Analytics"));
+const Analytics = lazy(() => import("@/pages/Admin/analytics"));
 
 export const adminSidebarItems: ISidebarItem[] = [
   {
